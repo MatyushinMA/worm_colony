(FP) Colony: attack fix + emplacing with weights Environment: spike aoe damage Memory: random batch indices fix Thread: normalization, breeding, learning, stats, attack bug fix Utils: commentary and hyperparameters refactoring Visual: distributions, stats, graphs Worm: init with weights, memo fix, learn fix, feed fix, breed mechanics Release!

Thread.py
@@ -69,8 +69,24 @@ class Thread:
         self.params['tick'] += 1
         self.environment.tick()
         self.colony.tick()
+        self.stats = {
+            'time' : self.params['tick'],
+            'breedings' : 0,
+            'crazy_actions' : 0,
+            'attacks' : 0,
+            'deaths' : 0,
+            'resources_exhaustion' : 0,
+            'population' : len(self.colony),
+            'world_lifespan' : self.params['world_lifespan'],
+            'food_eaten' : 0,
+            'spikes_hit' : 0,
+            'food_spawned' : 0,
+            'spikes_spawned' : 0
+        }
 
     def _is_alive(self):
+        if len(self.colony) == 0:
+            return False
         if self.params['tick'] <= self.params['world_lifespan']:
             return True
         else:
@@ -141,35 +157,44 @@ class Thread:
                 new_worm_view = np.rot90(new_worm_view, axes=(1, 0))
         return new_worm_view
 
+    def _normalize_worm_view(self, worm_view):
+        new_worm_view = worm_view
+        for i in range(3):
+            mean = worm_view[:,:,i].mean()
+            std = worm_view[:,:,i].std()
+            new_worm_view[:,:,i] -= mean
+            if std > 0:
+                new_worm_view[:,:,i] /= std
+        return new_worm_view
+
     def _update_worm_position(self, position, action):
         turn = action[0]
         move = action[1]
+        new_position = position
         if turn > EPS: # turn left
-            position[2] -= 1
-            position[2] %= 4
+            new_position[2] -= 1
+            new_position[2] %= 4
         elif turn < -EPS: # turn right
-            position[2] += 1
-            position[2] %= 4
+            new_position[2] += 1
+            new_position[2] %= 4
         move_value = 0
         if move > EPS: # move forward
             move_value = self.params['worm_speed']
         elif move < -EPS: # move backward
             move_value = -self.params['worm_speed']
         # actual moving
-        if position[2] == ORIENTATIONS['top']:
-            position[1] -= move_value
-            position[1] %= self.params['world_height']
-        elif position[2] == ORIENTATIONS['bottom']:
-            position[1] += move_value
-            position[1] %= self.params['world_height']
-        elif position[2] == ORIENTATIONS['left']:
-            position[0] -= move_value
-            position[0] %= self.params['world_width']
-        elif position[2] == ORIENTATIONS['right']:
-            position[0] += move_value
-            position[0] %= self.params['world_width']
+        if new_position[2] == ORIENTATIONS['top']:
+            new_position[1] -= move_value
+        elif new_position[2] == ORIENTATIONS['bottom']:
+            new_position[1] += move_value
+        elif new_position[2] == ORIENTATIONS['left']:
+            new_position[0] -= move_value
+        elif new_position[2] == ORIENTATIONS['right']:
+            new_position[0] += move_value
 
-        return position
+        new_position[0] %= self.params['world_width']
+        new_position[1] %= self.params['world_height']
+        return new_position
 
     def _colony_interaction(self, worm, attack):
         worm_position = worm.get_position()
@@ -177,12 +202,13 @@ class Thread:
         int_worm = None
         if self.world_view[worm_x, worm_y, 0] > 0:
             # TODO: Interaction with tails (they live on sphere)
-            int_worm = self.colony.get_worm_by_position(worm_x, worm_y)
+            int_worm = self.colony.get_worm_by_position(worm_x, worm_y, except_for=worm.get_id())
         if int_worm:
             if attack > EPS: # if worm decided to attack
                 old_health = int_worm.get_health()
                 new_health = old_health - WORM_DAMAGE
                 int_worm.set_health(new_health)
+                self.stats['attacks'] += 1
 
     def _food_interaction(self, worm, attack):
         worm_position = worm.get_position()
@@ -195,8 +221,10 @@ class Thread:
             old_health = worm.get_health()
             new_health = old_health + restore
             worm.set_health(new_health)
+            self.stats['food_eaten'] += 1
             if attack > EPS: # if worm decided to attack
                 int_food.eat() # food gets double hit
+                self.stats['food_eaten'] += 1
 
     def _spike_interaction(self, worm, attack):
         worm_position = worm.get_position()
@@ -209,6 +237,7 @@ class Thread:
             old_health = worm.get_health()
             new_health = old_health - SPIKE_DAMAGE
             worm.set_health(new_health)
+            self.stats['spikes_hit'] += 1
             if attack > EPS: # if worm decided to attack
                 int_spike.hit() # spike gets double hit
 
@@ -217,23 +246,28 @@ class Thread:
         self._food_interaction(worm, attack)
 
     def _spawn_spike(self):
-        if self.params['tick'] % SPAWN_TIMES['spike'] == 0:
-            x_pos = npr.randint(0, self.params['world_width'])
-            y_pos = npr.randint(0, self.params['world_height'])
-            self.environment.emplace_spike(x_pos, y_pos)
+        if self.params['tick'] % self.params['spike_spawn_time'] == 0:
+            for _ in range(self.params['spike_spawn_amount']):
+                x_pos = npr.randint(0, self.params['world_width'])
+                y_pos = npr.randint(0, self.params['world_height'])
+                self.environment.emplace_spike(x_pos, y_pos)
+                self.stats['spikes_spawned'] += 1
 
     def _spawn_food(self):
-        if self.params['tick'] % SPAWN_TIMES['food'] == 0:
-            x_pos = npr.randint(0, self.params['world_width'])
-            y_pos = npr.randint(0, self.params['world_height'])
-            self.environment.emplace_food(x_pos, y_pos)
+        if self.params['tick'] % self.params['food_spawn_time'] == 0:
+            for _ in range(self.params['food_spawn_amount']):
+                x_pos = npr.randint(0, self.params['world_width'])
+                y_pos = npr.randint(0, self.params['world_height'])
+                self.environment.emplace_food(x_pos, y_pos)
+                self.stats['food_spawned'] += 1
 
     def _spawn_worm(self):
-        if self.params['tick'] % SPAWN_TIMES['worm'] == 0:
-            x_pos = npr.randint(0, self.params['world_width'])
-            y_pos = npr.randint(0, self.params['world_height'])
-            orient = npr.randint(0, 4)
-            self.colony.emplace_worm(x_pos, y_pos, orient)
+        if self.params['tick'] % self.params['worm_spawn_time'] == 0:
+            for _ in range(self.params['worm_spawn_amount']):
+                x_pos = npr.randint(0, self.params['world_width'])
+                y_pos = npr.randint(0, self.params['world_height'])
+                orient = npr.randint(0, 4)
+                self.colony.emplace_worm(x_pos, y_pos, orient)
 
     def _spawn(self):
         self._spawn_spike()
@@ -282,6 +316,7 @@ class Thread:
         worm_adequacy = self.params['worm_adequacy'] + worm_expirience
         if odds > worm_adequacy: # time for inadequacy
             crazy_action = npr.uniform(0, 1, 3)
+            self.stats['crazy_actions'] += 1
             return crazy_action
         return action
 
@@ -290,32 +325,58 @@ class Thread:
             for worm in self.colony:
                 worm.learn()
 
+    def _breed(self, worm):
+        if worm.get_time() < self.params['breeding_age'] or worm.did_bred():
+            return
+        x0, y0, or0 = worm.get_position()
+        breed = self.colony.get_worm_by_position(x0, y0, except_for=worm.get_id())
+        if breed == None:
+            return
+        if breed.get_time() < self.params['breeding_age'] or breed.did_bred():
+            return
+        sd1 = worm.get_state_dict()
+        sd2 = breed.get_state_dict()
+        nsd = {}
+        for k in sd1:
+            l = npr.uniform()
+            new_weight = l*sd1[k] + (1 - l)*sd2[k]
+            nsd[k] = new_weight
+        x = npr.randint(x0 - 10, x0 + 10)
+        y = npr.randint(y0 - 10, y0 + 10)
+        orient = npr.randint(0, 4)
+        self.colony.emplace_worm(x, y, orient, nsd)
+        worm.breed_restore()
+        breed.breed_restore()
+        self.stats['breedings'] += 1
+
     def _run(self):
         while(self._is_alive()):
             self._tick()
-            self.visual.show_positions(self.colony, self.environment)
             self.world_view = self._render_world()
             for worm in self.colony:
                 worm_position = list(worm.get_position())
-                # print("Worm %d, position (%d, %d), health %d" % (worm.get_id(), worm_position[0], worm_position[1], worm.get_health()))
                 vb = self._get_worm_view(worm_position)
                 worm_view = self.__fill_worm_view(vb)
                 worm_view = self._rotate_worm_view(worm_view, worm_position[2])
+                worm_view = self._normalize_worm_view(worm_view)
                 action = worm(worm_view) # feed worm view to worm
                 action = self._epsilon_rand(action, worm.get_time())
-                print(action)
                 attack = action[2]
-                self._update_worm_position(worm_position, action)
+                worm_position = self._update_worm_position(worm_position, action)
                 worm.set_position(*worm_position)
                 self._colony_interaction(worm, attack)
                 self._environment_interaction(worm, attack)
             for worm in self.colony:
+                if self.params['breeding']:
+                    self._breed(worm)
+                worm.restore()
                 worm.memorize()
             if self.params['learning']:
                 self._learn()
-            self.colony.clean_up()
-            self.environment.clean_up()
+            self.stats['deaths'] = self.colony.clean_up()
+            self.stats['resources_exhaustion'] = self.environment.clean_up()
             self._spawn()
+            self.visual.show(self.colony, self.environment, self.stats)
             if self.params['visual_debug_show']:
                 sleep(RENDER_DELAY*(10**(-3)))
         self.visual.clear()

Visual.py
@@ -17,20 +17,40 @@ class Visual:
         self.height = self.params['world_height']*self.params['height_scale']
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
         if self.params['save_recap']:
-            self.out = cv2.VideoWriter('./recaps/%s.avi' % str(time.time()), fourcc, self.params['fps'], (self.width,self.height))
+            self.out = cv2.VideoWriter('./recaps/%s.avi' % str(time.time()), fourcc, self.params['fps'], (3*self.width, 3*self.height))
+        self.stats = {}
+        self.long_stats = {}
 
-    def show_positions(self, colony, environment):
-        frame = np.zeros((self.width, self.height, 3), np.uint8)
-        frame.fill(255)
-        for w in colony:
+    def show(self, colony, environment, stats):
+        self.colony = colony
+        self.environment = environment
+        self.stats = stats
+        for key in self.stats:
+            if key in self.long_stats.keys():
+                self.long_stats[key].append(self.stats[key])
+            else:
+                self.long_stats[key] = []
+        self.frame = np.zeros((self.height, self.width, 3), np.uint8)
+        self.frame.fill(255)
+        self.show_positions()
+        self.show_stats()
+        self.show_graphs()
+        if self.params['save_recap']:
+            self.out.write(self.frame)
+        if self.params['debug_show']:
+            cv2.imshow('frame', self.frame)
+            cv2.waitKey(RENDER_DELAY)
+
+    def show_positions(self):
+        for w in self.colony:
             worm_position = w.get_position()
             worm_x, worm_y, orient = worm_position[:]
             x1 = worm_x*self.params['width_scale']
             x2 = (worm_x+1)*self.params['width_scale']
             y1 = worm_y*self.params['height_scale']
             y2 = (worm_y+1)*self.params['height_scale']
-            cv2.rectangle(frame, (x1, y1), (x2, y2), self.params['worm_draw_color'], 3)
-        for el in environment:
+            cv2.rectangle(self.frame, (x1, y1), (x2, y2), self.params['worm_draw_color'], 3)
+        for el in self.environment:
             el_position = el.get_position()
             el_x, el_y = el_position[:2]
             x1 = el_x*self.params['width_scale']
@@ -40,12 +60,165 @@ class Visual:
             draw_color = self.params['spike_draw_color']
             if isinstance(el, Food):
                 draw_color = self.params['food_draw_color']
-            cv2.rectangle(frame, (x1, y1), (x2, y2), draw_color, 3)
-        if self.params['save_recap']:
-            self.out.write(frame)
-        if self.params['debug_show']:
-            cv2.imshow('frame', frame)
-            cv2.waitKey(RENDER_DELAY)
+            cv2.rectangle(self.frame, (x1, y1), (x2, y2), draw_color, 3)
+
+    def show_stats(self):
+        self.stats_frame = np.zeros((self.height, 2*self.width, 3), np.uint8)
+        self.stats_frame.fill(255)
+        cv2.line(self.stats_frame, (1, 0), (1, self.height),
+                                    (0, 0, 0), 3)
+        health_distribution = [0 for i in range(101)]
+        saturation_distribution = [0 for i in range(101)]
+        age_distribution = [0 for i in range(self.colony.max_time + 1)]
+        for w in self.colony:
+            w_health = int(round(w.get_health()))
+            w_saturation = int(round(w.get_saturation()))
+            w_age = w.get_time()
+            if w_health >= 0:
+                health_distribution[w_health] += 1
+            if w_saturation >= 0:
+                saturation_distribution[w_saturation] += 1
+            if w_age >= 0:
+                age_distribution[w_age] += 1
+        health_frame = {
+            'name' : 'Health distribution',
+            'y1' : 0,
+            'y2' : self.height // 4,
+            'max_value' : 100,
+            'min_value' : 0
+        }
+        saturation_frame = {
+            'name' : 'Saturation distribution',
+            'y1' : self.height // 4,
+            'y2' : 2*self.height // 4,
+            'max_value' : 100,
+            'min_value' : 0
+        }
+        age_frame = {
+            'name' : 'Age distribution',
+            'y1' : 2*self.height // 4,
+            'y2' : 3*self.height // 4,
+            'max_value' : self.colony.max_time,
+            'min_value' : 0
+        }
+        info_frame = {
+            'name' : 'Statistics',
+            'y1' : 3*self.height // 4,
+            'y2' : self.height
+        }
+        self.show_distribution(health_distribution, health_frame)
+        self.show_distribution(saturation_distribution, saturation_frame)
+        self.show_distribution(age_distribution, age_frame)
+        self.show_info(info_frame)
+        self.frame = np.concatenate((self.frame, self.stats_frame), axis=1)
+
+    def show_distribution(self, distribution, frame):
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        block_height = frame['y2'] - frame['y1']
+        block_width = int(1.95*self.width // len(distribution))
+        font_size = block_height/float(2*110)
+        title = int(block_height * 0.1)
+        body = int(block_height * 0.6)
+        cv2.putText(self.stats_frame, frame['name'], (int(0.05*2*self.width), frame['y1'] + title),
+                                                      font, font_size, (0, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(self.stats_frame, '%d' % frame['min_value'], (0, frame['y2'] - title),
+                                            font, font_size, (0, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(self.stats_frame, '%d' % frame['max_value'], (int(0.95*2*self.width), frame['y2'] - title),
+                                            font, font_size, (0, 0, 0), 2, cv2.LINE_AA)
+        top_value = max(distribution)
+        if top_value == 0:
+            cv2.rectangle(self.stats_frame, (block_width, frame['y2'] - 2*title),
+                                            (block_width*len(distribution), frame['y2'] - 2*title),
+                                            (0, 0, 0), 3)
+        else:
+            for horizontal, value in enumerate(distribution):
+                height = int(body*float(value)/top_value)
+                cv2.rectangle(self.stats_frame, ((horizontal + 1)*block_width, frame['y2'] - 2*title),
+                                                ((horizontal + 2)*block_width, frame['y2'] - 2*title - height),
+                                                (0, 0, 0), 3)
+
+    def show_info(self, frame):
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        block_width = int(float(2*self.width) / (len(self.stats.keys()) / float(3)))
+        block_height = (frame['y2'] - frame['y1']) // 3
+        offset = int(0.025*2*self.width)
+        font_size = block_height/float(110)
+        for i, stat_name in enumerate(self.stats):
+            stat_value = self.stats[stat_name]
+            stat_string = '%s : %d' % (stat_name, stat_value)
+            horizontal = i // 3
+            vertical = i % 3
+            cv2.putText(self.stats_frame, stat_string, (offset + block_width*horizontal,
+                                                        frame['y1'] + int(block_height*(vertical + 0.5))),
+                                                        font, font_size, (0, 0, 0), 2, cv2.LINE_AA)
+
+    def show_graphs(self):
+        if self.stats['time'] == 1:
+            return
+        self.graphs_frame = np.zeros((2*self.height, 3*self.width, 3), np.uint8)
+        self.graphs_frame.fill(255)
+        bd_frame = {
+            'name' : 'Birth-death-population',
+            'y1' : 0,
+            'y2' : 2*self.height // 4,
+            'names' : ['breedings', 'deaths', 'population'],
+            'colors' : [(0, 255, 0), (0, 0, 255), (255, 0, 0)]
+        }
+        fs_frame = {
+            'name' : 'Food-spike hits',
+            'y1' : 2*self.height // 4,
+            'y2' : 4*self.height // 4,
+            'names' : ['food_eaten', 'spikes_hit'],
+            'colors' : [self.params['food_draw_color'], self.params['spike_draw_color']]
+        }
+        spawn_frame = {
+            'name' : 'Food-spike spawns',
+            'y1' : 4*self.height // 4,
+            'y2' : 3*2*self.height // 4,
+            'names' : ['food_spawned', 'spikes_spawned'],
+            'colors' : [self.params['food_draw_color'], self.params['spike_draw_color']]
+        }
+        crazy_frame = {
+            'name' : 'Crazy actions',
+            'y1' : 3*2*self.height // 4,
+            'y2' : 2*self.height,
+            'names' : ['crazy_actions'],
+            'colors' : [(13, 92, 203)]
+        }
+        self.show_graph(bd_frame)
+        self.show_graph(fs_frame)
+        self.show_graph(spawn_frame)
+        self.show_graph(crazy_frame)
+        self.frame = np.concatenate((self.frame, self.graphs_frame), axis=0)
+
+    def show_graph(self, frame):
+        cv2.line(self.graphs_frame, (0, frame['y1'] + 1), (3*self.width, frame['y1'] + 1),
+                                    (0, 0, 0), 4)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        block_height = int((frame['y2'] - frame['y1'])*0.8)
+        block_width = int(3*0.90*self.width/float(self.stats['world_lifespan']))
+        offset = int(3*0.05*self.width)
+        font_size = block_height/float(440)
+        bottom_offset = int((frame['y2'] - frame['y1'])*0.05)
+        cv2.putText(self.graphs_frame, frame['name'], (int(3*0.1*self.width),
+                                                    frame['y1'] + int(0.1*block_height)),
+                                                    font, font_size, (0, 0, 0), 2, cv2.LINE_AA)
+        top = 0
+        for name in frame['names']:
+            top = max(top, max(self.long_stats[name]))
+        if top == 0:
+            top = 100
+        for draw_color, name in zip(frame['colors'], frame['names']):
+            prev_value = self.long_stats[name][0]
+            prev_horizontal = 0
+            for new_horizontal, new_value in enumerate(self.long_stats[name]):
+                x1 = int(offset + prev_horizontal*block_width)
+                x2 = int(offset + new_horizontal*block_width)
+                y1 = int(frame['y2'] - bottom_offset - (float(prev_value)/top)*block_height)
+                y2 = int(frame['y2'] - bottom_offset - (float(new_value)/top)*block_height)
+                cv2.line(self.graphs_frame, (x1, y1), (x2, y2), draw_color, 2)
+                prev_value = new_value
+                prev_horizontal = new_horizontal
 
     def clear(self):
         if self.params['save_recap']:

Worm.py
@@ -8,15 +8,18 @@ import torch.nn as nn
 import numpy.random as npr
 
 class Worm:
-    def __init__(self, id, x, y, orientation=0):
+    def __init__(self, id, x, y, orientation=0, weights=None):
         self.id = id
-        self.health = 100
+        self.health = 100.
         self.time = 0
         self.power = 1
-        self.saturation = 100
+        self.saturation = 100.
         self.orient = orientation
+        self.bred = False
 
         self.net = WormNET()
+        if weights:
+            self.net.load_state_dict(weights)
         self.storage = Storage(WORM_MEMORY_SIZE)
         self.optimizer = torch.optim.Adam(self.net.parameters(), INITIAL_LR)
         self.loss_fn = nn.CrossEntropyLoss()
@@ -39,11 +42,14 @@ class Worm:
         return pred
 
     def memorize(self):
-        self._state['health'] = self.health - self._state['health']
-        self._state['saturation'] = self.saturation - self._state['saturation']
-        self.storage.push_memo(self._view, self._act, self._state)
+        if self.time > 0:
+            self._state['health'] = self.health - self._state['health']
+            self._state['saturation'] = self.saturation - self._state['saturation']
+            self.storage.push_memo(self._view, self._act, self._state)
 
     def learn(self):
+        if self.time == 0:
+            return
         self.net.train()
         lbs = npr.randint(1, LEARN_BATCH_SIZE)
         learn_batch = self.storage.batch(lbs)
@@ -81,17 +87,41 @@ class Worm:
                     param_group['lr'] = lr
 
                 prob, _ = self.net(view)
-                loss = torch.zeros(1, requires_grad=True)
+                loss1 = None
+                flag1 = False
+                loss2 = None
+                flag2 = False
+                loss3 = None
+                flag3 = False
                 if target[0].item() >= 0:
-                    loss += self.loss_fn(prob[0], target[0])
+                    loss1 = self.loss_fn(prob[0], target[0])
+                    flag1 = True
                 if target[1].item() >= 0:
-                    loss += self.loss_fn(prob[1], target[1])
+                    loss2 = self.loss_fn(prob[1], target[1])
+                    flag2 = True
                 if target[2].item() >= 0:
-                    loss += self.loss_fn(prob[2], target[2])
+                    loss3 = self.loss_fn(prob[2], target[2])
+                    flag3 = True
 
                 self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
+                if flag1:
+                    loss = loss1
+                    if flag2:
+                        loss += loss2
+                    if flag3:
+                        loss += loss3
+                    loss.backward()
+                    self.optimizer.step()
+                elif flag2:
+                    loss = loss2
+                    if flag3:
+                        loss += loss3
+                    loss.backward()
+                    self.optimizer.step()
+                elif flag3:
+                    loss = loss3
+                    loss.backward()
+                    self.optimizer.step()
 
     def get_id(self):
         return self.id
@@ -107,6 +137,7 @@ class Worm:
 
     def set_health(self, h):
         self.health = h
+        self.health = max(min(self.health, 100), 0)
 
     def get_saturation(self):
         return self.saturation
@@ -119,6 +150,24 @@ class Worm:
 
     def tick(self):
         self.time += 1
+        self.bred = False
+
+    def restore(self):
+        self.saturation -= 5.
+        self.saturation = max(self.saturation, 0.)
+        if self.saturation <= 30.:
+            self.health -= 5.
+            self.health = max(self.health, 0.)
+        elif self.saturation >= 60.:
+            self.health += 5.
+            self.health = min(self.health, 100.)
+
+    def breed_restore(self):
+        self.health = 100.
+        self.bred = True
+
+    def did_bred(self):
+        return self.bred
 
     def get_position(self):
         return (self.x, self.y, self.orient)
@@ -127,3 +176,6 @@ class Worm:
         self.x = x
         self.y = y
         self.orient = orientation
+
+    def get_state_dict(self):
+        return self.net.state_dict()